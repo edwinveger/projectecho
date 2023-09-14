0b8040d1d2caf083b837f4a5c3629ee783aa66e1
Edwin <info@pierrecode.nl>
2023-09-14 15:12:33 +0200

    Transmit binary signal with ASK on carrier frequency

--- Arduino/prototype_beacon/prototype_beacon.ino
+++ Arduino/prototype_beacon/prototype_beacon.ino
@@ -3,6 +3,12 @@ const int button2Pin = 4;  // Button 2 pin
 const int potentiometerPin = A0; // Potentiometer pin
 const int speakerPin = 8;  // Speaker pin
 
+//const unsigned long bitDuration = 1000; // 0.001 seconds
+const unsigned long bitDuration = 100000; // 0.1 seconds
+
+// Define the 4-bit binary value to be modulated
+const byte dataToModulate = 0b1010; // Change this to your desired 4-bit binary value
+
 bool soundEnabled = false;  // Global flag to enable/disable sound
 int volume = 0;           // Variable to store volume
 int frequencyIndex = 0;   // Index to select frequencies from the list
@@ -20,6 +26,23 @@ void setup() {
 }
 
 void loop() {
+  handleButton1();
+  handleButton2();
+
+  // Read the potentiometer value and map it to volume
+  //int potValue = analogRead(potentiometerPin);
+  // Serial.println(potValue);
+  //volume = map(potValue, 0, 1023, 0, 255);
+
+  // Generate and play sound if sound is enabled
+  if (soundEnabled) {
+    playSound();
+  } else {
+    noTone(speakerPin); // Turn off the sound if disabled
+  }
+}
+
+void handleButton1() { 
   // Read the state of Button 1
   bool button1State = digitalRead(button1Pin);
 
@@ -30,8 +53,10 @@ void loop() {
     Serial.println(soundEnabled);
   }
   prevButton1State = button1State;
+}
 
-  // Read the state of Button 2
+void handleButton2() {
+   // Read the state of Button 2
   bool button2State = digitalRead(button2Pin);
 
   // Change frequency if Button 2 is pressed
@@ -41,17 +66,35 @@ void loop() {
     Serial.println(frequencies[frequencyIndex]);
   }
   prevButton2State = button2State;
+}
 
-  // Read the potentiometer value and map it to volume
-  int potValue = analogRead(potentiometerPin);
-  // Serial.println(potValue);
-  volume = map(potValue, 0, 1023, 0, 255);
+void playSound() { 
+  // Modulate and emit the signal
+  for (int i = 0; i < 4; i++) {
+    int bit = (dataToModulate >> i) & 0x01; // Extract each bit from the 4-bit data
 
-  // Generate and play sound if sound is enabled
-  if (soundEnabled) {
-    int frequency = frequencies[frequencyIndex];
-    tone(speakerPin, frequency);
-  } else {
-    noTone(speakerPin); // Turn off the sound if disabled
+    // Modulate and emit the bit
+    modulateAndEmit(bit);
   }
+
+  // Delay before transmitting the next set of bits
+  delay(1000); // Adjust as needed
+}
+
+unsigned int carrierFrequency() { 
+  return frequencies[frequencyIndex];
+}
+
+void modulateAndEmit(int bit) {
+  unsigned long startTime = micros();
+
+  while (micros() - startTime < bitDuration) {
+    if (bit == 1) {
+      tone(speakerPin, carrierFrequency()); // Transmit the carrier frequency for a 1 bit
+    } else {
+      noTone(speakerPin); // Turn off the carrier signal for a 0 bit
+    }
+  }
+
+  noTone(speakerPin);
 }
